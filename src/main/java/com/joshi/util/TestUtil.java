c25ee5fe32cec273b6ca48ba8f7d8568410a4bc0
Laxmi Kant Joshi <lkjoshi@hotmail.co.uk>
2021-02-17 23:34:25 +0000

    first git commit for my selenium code

---
+++ src/main/java/com/joshi/util/TestUtil.java
@@ -0,0 +1,7 @@
+package com.joshi.util;
+
+public class TestUtil {
+    public void testUtil(){
+        System.out.println("Test Util");
+    }
+}
---
+++ src/test/java/com/joshi/tests/LogiTest.java
@@ -0,0 +1,11 @@
+package com.joshi.tests;
+
+import org.testng.annotations.Test;
+
+public class LogiTest {
+    @Test
+    public void test1(){
+        System.out.println("Test Method ");
+
+    }
+}
